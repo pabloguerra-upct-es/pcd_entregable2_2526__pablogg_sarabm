9293f599091417d74796fa779458b4090f923776
pabloguerra-upct-es <pablo.guerra@edu.upct.es>
2026-05-01 19:19:32 +0200

    Cambios en el patron Singleton

--- streaming_musical.py
+++ streaming_musical.py
@@ -65,7 +65,7 @@ class Artista(ElementoCatalogo):
         self.__canciones = canciones
         self.__fecha_nacimiento = fecha_nacimiento
 
-    def obtenerCanciones(self):
+    def obtenerCanciones(self) -> list:
         return self.__canciones
 
     def obtenerCaracteristicas(self) -> dict:
@@ -92,9 +92,9 @@ class ListaReproduccion:
         
         self.__canciones = canciones
         self.__nombre = nombre
-        self.__fechs_creacion = fecha_creacion
+        self.__fecha_creacion = fecha_creacion
 
-    def obtenerCanciones(self):
+    def obtenerCanciones(self) -> list:
         return self.__canciones
 
     def obtenerCaracteristicas(self) -> dict:
@@ -129,10 +129,10 @@ class Sesion:
             
         self.__canciones.append(c)
 
-    def obtenerCanciones(self):
+    def obtenerCanciones(self) -> list:
         return self.__canciones
     
-    def obtenerCaracteristicas(self, m:"Manejador"): 
+    def obtenerCaracteristicas(self, m:"Manejador") -> dict: 
         if not isinstance(m, Manejador):
             raise TypeError("m debe pertenecer a la clase Manejador")
         
@@ -151,7 +151,7 @@ class Manejador(ABC):
         
         self._siguiente = siguiente
 
-    def setSiguiente(self, m: "Manejador"):
+    def setSiguiente(self, m: "Manejador") -> Manejador:
         """Permite cambiar o establecer el siguiente manejador en la cadena."""
         if not isinstance(m, Manejador):
             raise TypeError("El nuevo manejador debe pertenecer a la clase Manejador")
@@ -310,13 +310,13 @@ class ServicioStreaming:
         self.__artistas = artistas
         self.__listas_repro = listas_repro
 
-    def getCanciones(self):
+    def getCanciones(self) -> list:
         return self.__canciones
     
-    def getArtistas(self):
+    def getArtistas(self) -> list:
         return self.__artistas
     
-    def getListasRepro(self):
+    def getListasRepro(self) -> list:
         return self.__listas_repro
 
 class EstrategiaBusqueda(ABC):
@@ -337,7 +337,7 @@ class Alfabetico(EstrategiaBusqueda):
         
         if not isinstance(sesion, Sesion):
             raise TypeError("sesion debe pertenecer a la clase Sesion")
-
+        
         catalogo_completo = set(itertools.chain(
             catalogo.getCanciones(),
             *(artista.obtenerCanciones() for artista in catalogo.getArtistas()),
@@ -349,9 +349,9 @@ class Alfabetico(EstrategiaBusqueda):
             key=lambda c: c.__titulo.lower()
         )
 
-        estadisticas_sesion = sesion.obtenerCaracteristicas()
+        estadisticas_sesion = sesion.obtenerCaracteristicas(ManejadorSonoro(ManejadorSentimental()))
 
-        def coincide(cancion):
+        def coincide(cancion) -> bool:
             caract_can = cancion.obtenerCaracteristicas()
             sonoras = caract_can.get("Caracteristicas Sonoras", {})
             sentimentales = caract_can.get("Caracteristicas Sentimentales", {})
@@ -391,16 +391,16 @@ class Temporal(EstrategiaBusqueda):
         items_ordenados = sorted(
             items_catalogo,
             key=lambda x: (
-                x._Cancion__fecha_creacion if isinstance(x, Cancion) else
-                x._ListaReproduccion__fecha_creacion if isinstance(x, ListaReproduccion) else
-                x._Artista__fecha_nacimiento if isinstance(x, Artista) else date.min
+                x.__fecha_creacion if isinstance(x, Cancion) else
+                x.__fecha_creacion if isinstance(x, ListaReproduccion) else
+                x.__fecha_nacimiento if isinstance(x, Artista) else date.min
             ),
             reverse=True
         )
 
         estadisticas_sesion = sesion.obtenerCaracteristicas(ManejadorSonoro(ManejadorSentimental()))
 
-        def coincide(item):
+        def coincide(item) -> bool:
             if isinstance(item, Cancion):
                 canciones_a_validar = [item]
 
@@ -430,6 +430,8 @@ class Temporal(EstrategiaBusqueda):
             if coincide(item):
                 return item
 
+        return None
+
 class Aleatorio(EstrategiaBusqueda):
     def buscar(self, catalogo: ServicioStreaming, sesion: Sesion):
         if not isinstance(catalogo, ServicioStreaming):
@@ -446,9 +448,9 @@ class Aleatorio(EstrategiaBusqueda):
         
         random.shuffle(canciones_lista) 
 
-        estadisticas_sesion = sesion.obtenerCaracteristicas()
+        estadisticas_sesion = sesion.obtenerCaracteristicas(ManejadorSonoro(ManejadorSentimental()))
 
-        def coincide(cancion):
+        def coincide(cancion) -> bool:
             caract_can = cancion.obtenerCaracteristicas()
             sonoras = caract_can.get("Caracteristicas Sonoras", {})
             sentimentales = caract_can.get("Caracteristicas Sentimentales", {})
@@ -481,7 +483,7 @@ class Recomendacion:
         
         self._elemento = elemento
 
-    def obtenerResultado(self):
+    def obtenerResultado(self) -> ElementoCatalogo:
         return self._elemento
 
 class DecoradorRecom(Recomendacion):
@@ -493,7 +495,7 @@ class DecoradorRecom(Recomendacion):
         
         self.__recomendacion = recomendacion
 
-    def obtenerResultado(self):
+    def obtenerResultado(self) -> ElementoCatalogo:
         return self.__recomendacion.obtenerResultado()
 
 class RecomArtista(DecoradorRecom):
@@ -543,7 +545,6 @@ class SistemaRecomendacion:
         if SistemaRecomendacion.__instancia is not None:
             raise SingletonException("Al ser un Singleton, debes usar primero getInstancia")       
         
-        SistemaRecomendacion.__instancia = self
         self.__estrategia = None
         self.__manejador_inicial = None
         self.__sesion = None
@@ -551,10 +552,10 @@ class SistemaRecomendacion:
 
     @classmethod
     def getInstancia(cls):
-        if cls._SistemaRecomendacion__instancia is None:
-            cls._SistemaRecomendacion__instancia = cls()
+        if cls.__instancia is None:
+            cls.__instancia = cls()
         
-        return cls._SistemaRecomendacion__instancia
+        return cls.__instancia
     
     def setEstrategia(self, e:EstrategiaBusqueda):
         if not isinstance(e, EstrategiaBusqueda):
@@ -595,7 +596,7 @@ class SistemaRecomendacion:
         
         cancion_encontrada = None
         for cancion in catalogo.getCanciones():
-            if cancion._Cancion__id == id:
+            if cancion.__id == id:
                 cancion_encontrada = cancion
 
         if not cancion_encontrada:
@@ -614,6 +615,10 @@ class SistemaRecomendacion:
             raise SesionError("Se deben inicializar los valores de estrategia y sesion")
         
         elemento_base = self.__estrategia.buscar(catalogo, self.__sesion)
+
+        if not elemento_base:
+            return None
+        
         recomendacion = Recomendacion(elemento_base)
 
         if self.__tipo_recomendacion == Artista:
